4f26e2efcd5fd225c16e4ea8a053b910a198a4b1
godspeedelbow <eelke@boezeman.com>
2015-04-25 18:31:00 +0200

    removed old demo config file

--- demo/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    SOURCE_DIR: __dirname + '/client/js',
-    SOURCEMAP_PREFIX: '/_sourcemap',
-    SOURCEMAP_PATH_PREFIX_REGEX: /^\/_sourcemap\//,
-    ORIGINAL_SOURCE_PATH_PREFIX: 'http://127.0.0.1:1337/_js',
-    ORIGINAL_SOURCE_PATH_PREFIX_REGEX: /^\/_js\//,
-};
